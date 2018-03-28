16b5b7639a8ac5a88199d53cfec82d569d7969c7
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-03-28 00:21:11 -0300

    Resized login button.

--- src/components/LoginButton/index.js
+++ src/components/LoginButton/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 const LoginButton = ({urlToAuth}) => (
-    <a href={urlToAuth} className="btn spotify-button">
+    <a href={urlToAuth} className="btn btn-lg spotify-button">
         <i className="fa fa-spotify"/>
     </a>
 );
