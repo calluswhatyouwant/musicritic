d615e9f8b98c032cbf7610a0de3fbbc17fda3263
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-04-07 01:38:51 -0300

    Added social buttons to the signup page.

--- src/components/signup/SignupPage.js
+++ src/components/signup/SignupPage.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
+import './signup.css';
 import SignupForm from './SignupForm';
 
 class SignupPage extends Component {
     render() {
         return (
             <div className="row justify-content-center">
-                <div className="col-sm-12 col-md-4">
+                <div className="col-5 col-sm-12 col-md-7 col-lg-5">
+                    <h1 className="text-center">Signup with</h1>
                     <SignupButtonGroup />
-                </div>
-                <div className="col-sm-12 col-md-4">
+                    <Separator />
                     <SignupForm />
                 </div>
             </div>
@@ -17,6 +19,32 @@ class SignupPage extends Component {
     }
 }
 
-const SignupButtonGroup = ({}) => <div></div>;
+const SignupButtonGroup = () => (
+    <div className="signup-btn-group row">
+        <div className="col-4">
+            <SignupButton name={'facebook'} />
+        </div>
+        <div className="col-4">
+            <SignupButton name={'twitter'} />
+        </div>
+        <div className="col-4">
+            <SignupButton name={'google'} />
+        </div>
+    </div>
+);
+
+const SignupButton = ({name}) => (
+    <Button className={`btn btn-block btn-signup btn-signup-${name} text-center`}>
+        <span className={`fa fa-${name}`} />
+    </Button>
+);
+
+const Separator = () => (
+    <div className="row separator text-center">
+        <div className="col-5"><hr /></div>
+        <div className="col-2">or</div>
+        <div className="col-5"><hr /></div>
+    </div>
+);
 
 export default SignupPage;
