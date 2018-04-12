ac8ed8fe79e48dee643c9b354257f97d523a40f9
José Renan <joserenansl99@gmail.com>
2018-04-12 15:09:39 -0300

    Initialized the configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Default values for the listed environment variables.
+ * To use a custom value, just add the environment variable on your OS
+ * using the names given on the following keys.
+ */
+module.exports = {
+    FIREBASE_API_KEY: '',
+    FIREBASE_AUTH_DOMAIN: '',
+    SERVER_BASE_URI: 'http://localhost:5000'
+}
