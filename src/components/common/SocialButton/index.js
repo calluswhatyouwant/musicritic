0a9ea3942daf26d5d76d6e4e256b0142ef3e1f02
JRobsonJr <jrobsonjr16@gmail.com>
2018-05-29 21:19:38 -0300

    Add onClick property to social buttons.

--- src/components/common/SocialButton/index.js
+++ src/components/common/SocialButton/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './social-button.css';
 
-const SocialButton = ({name, url}) => (
-    <a href={url} className={`btn btn-block btn-social btn-${name} text-center`}>
+const SocialButton = ({name, onClick}) => (
+    <a className={`btn btn-block btn-social btn-${name} text-center`} onClick={onClick}>
         <span className={`fa fa-${name}`} />
     </a>
 );
