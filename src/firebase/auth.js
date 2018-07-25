2ab545a4cdd466776a83d4b8e1bfae507c4872db
JRobsonJr <jrobsonjr16@gmail.com>
2018-07-24 22:17:08 -0300

    Remove unfinished and commented code

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -5,17 +5,4 @@ export const signInWithEmailAndPassword = (email, password) =>
 
 export const getCurrentUser = () => auth().currentUser;
 
-export const signInWithFacebook = () => {
-    const facebookProvider = new auth.FacebookAuthProvider();
-
-    auth().signInWithRedirect(facebookProvider).then(() => {
-        auth().getRedirectResult().then((result) => {
-            if (result.credential) {
-                // const token = result.credential.accessToken;
-            }
-            // const user = result.user;
-        }).catch(error => console.log(error.code));
-    });
-};
-
 export const getAccessToken = () => getCurrentUser().getIdToken();
