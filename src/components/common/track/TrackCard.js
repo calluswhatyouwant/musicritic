9c874b90ff50aac415f2f60b3d278bb08ae95c3d
JRobsonJr <jrobsonjr16@gmail.com>
2018-08-09 15:22:45 -0300

    Add handleClick property to TrackCard

--- src/components/common/track/TrackCard.js
+++ src/components/common/track/TrackCard.js
@@ -6,18 +6,32 @@ import { Track } from '../../../spotify/models';
 
 type Props = {
     track: Track,
+    handleClick: (track: Track) => void,
 };
 
-const TrackCard = ({ track }: Props) => (
-    <div className="card text-center">
-        <img className="card-img-top" src={track.album.imageUrl} alt="Top" />
-        <div className="card-body">
-            <h6 className="card-subtitle mb-2 text-muted text-truncate">
-                {track.name}
-            </h6>
-            <p className="card-text text-truncate">{track.stringArtists}</p>
+const TrackCard = ({ track, handleClick }: Props) => {
+    const clickableProps = {
+        onClick: handleClick,
+        tabIndex: 0,
+        onKeyPress: () => {},
+        role: 'button',
+    };
+
+    return (
+        <div className="card text-center" {...clickableProps}>
+            <img
+              className="card-img-top"
+              src={track.album.imageUrl}
+              alt="Top"
+            />
+            <div className="card-body">
+                <h6 className="card-subtitle mb-2 text-muted text-truncate">
+                    {track.name}
+                </h6>
+                <p className="card-text text-truncate">{track.stringArtists}</p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default TrackCard;
