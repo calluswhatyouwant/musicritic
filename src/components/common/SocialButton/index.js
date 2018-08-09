40cf1eb9b7b5f1ea13ca4c31059e353d4f48bd6d
JRobsonJr <jrobsonjr16@gmail.com>
2018-08-09 15:21:55 -0300

    Add content property to SocialButton

--- src/components/common/SocialButton/index.js
+++ src/components/common/SocialButton/index.js
@@ -7,19 +7,21 @@ import './social-button.css';
 type Props = {
     name: string,
     url?: string,
+    content?: string,
 };
 
-const SocialButton = ({ name, url }: Props) => (
+const SocialButton = ({ name, url, content }: Props) => (
     <a
       className={`btn btn-block btn-social btn-${name} text-center`}
       href={url}
     >
-        <span className={`fa fa-${name}`} />
+        {content} <span className={`fa fa-${name}`} />
     </a>
 );
 
 SocialButton.defaultProps = {
     url: '',
+    content: '',
 };
 
 export default SocialButton;
