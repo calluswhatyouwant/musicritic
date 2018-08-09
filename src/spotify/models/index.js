a739f1b622cef18a32380a6e41e062b769cf7a27
JRobsonJr <jrobsonjr16@gmail.com>
2018-08-09 15:25:44 -0300

    Add attributes to Track model and create AudioFeature model

--- src/spotify/models/index.js
+++ src/spotify/models/index.js
@@ -44,6 +44,8 @@ export class Track {
     imageUrl: string;
     name: string;
     explicit: boolean;
+    popularity: number;
+    spotifyUrl: string;
 
     constructor(trackJson: any) {
         if (trackJson) {
@@ -53,12 +55,25 @@ export class Track {
             this.id = trackJson.id;
             this.name = trackJson.name;
             this.explicit = trackJson.explicit;
+            this.popularity = trackJson.popularity;
+            this.spotifyUrl = trackJson.external_urls.spotify;
         }
     }
 
     get stringArtists() {
         const artistNames = this.artists.map(artist => artist.name);
         return artistNames.join(', ');
+    }
+
+    get popularityRank() {
+        if (this.popularity >= 90) {
+            return 'HIGHLY POPULAR';
+        } else if (this.popularity >= 75) {
+            return 'POPULAR';
+        } else if (this.popularity >= 50) {
+            return 'SOMEWHAT POPULAR';
+        }
+        return '';
     }
 }
 
@@ -73,5 +88,99 @@ export class Playlist {
         this.imageUrl = playlistJson.images[0].url;
         this.name = playlistJson.name;
         this.owner = playlistJson.owner.display_name || playlistJson.owner.id;
+    }
+}
+
+export class AudioFeatures {
+    danceability: number;
+    energy: number;
+    key: number;
+    loudness: number;
+    mode: number;
+    speechiness: number;
+    acousticness: number;
+    instrumentalness: number;
+    liveness: number;
+    valence: number;
+    tempo: number;
+
+    constructor(audioFeaturesJson: any) {
+        if (audioFeaturesJson) {
+            this.danceability = audioFeaturesJson.danceability;
+            this.energy = audioFeaturesJson.energy;
+            this.key = audioFeaturesJson.key;
+            this.loudness = audioFeaturesJson.loudness;
+            this.mode = audioFeaturesJson.mode;
+            this.speechiness = audioFeaturesJson.speechiness;
+            this.acousticness = audioFeaturesJson.acousticness;
+            this.instrumentalness = audioFeaturesJson.instrumentalness;
+            this.liveness = audioFeaturesJson.liveness;
+            this.valence = audioFeaturesJson.valence;
+            this.tempo = audioFeaturesJson.tempo;
+        }
+    }
+
+    static rankFeatureLikelihood(feature: number) {
+        if (feature > 0.9) {
+            return 'MOST CERTAINLY';
+        } else if (feature > 0.8) {
+            return 'VERY LIKELY';
+        } else if (feature > 0.7) {
+            return 'LIKELY';
+        }
+        return '';
+    }
+
+    static rankFeature(feature: number) {
+        if (feature > 0.85) {
+            return 'VERY HIGH';
+        } else if (feature > 0.75) {
+            return 'HIGH';
+        } else if (feature > 0.5) {
+            return 'MEDIUM HIGH';
+        } else if (feature > 0.25) {
+            return 'MEDIUM LOW';
+        } else if (feature > 0.15) {
+            return 'LOW';
+        }
+        return 'VERY LOW';
+    }
+
+    get tags() {
+        const tags = [];
+
+        tags.push(
+            {
+                feature: 'TEMPO',
+                value: `${Math.round(this.tempo)} BPM`,
+            },
+            {
+                feature: 'ACOUSTIC',
+                value: AudioFeatures.rankFeatureLikelihood(this.acousticness),
+            },
+            {
+                feature: 'DANCEABILITY',
+                value: AudioFeatures.rankFeature(this.danceability),
+            },
+            {
+                feature: 'ENERGY',
+                value: AudioFeatures.rankFeature(this.danceability),
+            },
+            {
+                feature: 'INSTRUMENTAL',
+                value: AudioFeatures
+                    .rankFeatureLikelihood(this.instrumentalness),
+            },
+            {
+                feature: 'LIVE',
+                value: AudioFeatures.rankFeatureLikelihood(this.liveness),
+            },
+            {
+                feature: 'POSITIVENESS',
+                value: AudioFeatures.rankFeature(this.valence),
+            },
+        );
+
+        return tags;
     }
 }
