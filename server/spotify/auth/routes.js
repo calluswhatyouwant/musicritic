6a8bac569d065920001d5917385ac9f0ab688f71
José Renan <joserenansl99@gmail.com>
2018-08-12 02:09:15 -0300

    Fix eslint problems

--- server/spotify/auth/routes.js
+++ server/spotify/auth/routes.js
@@ -8,7 +8,7 @@ import {
     getSpotifyAuthUrl,
     getHostUrl,
     getClientUrl,
-    getTokenReqOptions
+    getTokenReqOptions,
 } from './util';
 
 const router = express.Router();
@@ -23,13 +23,31 @@ router.get('/login', (req, res) => {
             client_id: config.spotify.clientId,
             scope: config.spotify.scope,
             redirect_uri: getHostUrl('/auth/callback'),
-            state: state
-        })
-    );
+            state,
+        }));
 });
 
+const requestForSpotifyUserToken = (req, res) => {
+    res.clearCookie(stateKey);
+    const { code } = req.query;
+    const tokenRequestOptions = getTokenReqOptions({
+        code,
+        redirect_uri: getHostUrl('/auth/callback'),
+        grant_type: 'authorization_code',
+    });
+    request.post(tokenRequestOptions, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            res.redirect(getClientUrl(`${config.client.successPath}`
+                + `/${body.access_token}/${body.refresh_token}`));
+        } else {
+            res.redirect(getClientUrl(config.client.errorPath)
+                + querystring.stringify({ error: 'invalid_token' }));
+        }
+    });
+};
+
 router.get('/callback', (req, res) => {
-    const state = req.query.state;
+    const { state } = req.query;
     const storedState = req.cookies ? req.cookies[stateKey] : null;
     if (!state || state !== storedState) {
         res.redirect(getClientUrl(config.client.errorPath)
@@ -38,24 +56,5 @@ router.get('/callback', (req, res) => {
         requestForSpotifyUserToken(req, res);
     }
 });
-
-const requestForSpotifyUserToken = (req, res) => {
-    res.clearCookie(stateKey);
-    const code = req.query.code;
-    const tokenRequestOptions = getTokenReqOptions({
-        code: code,
-        redirect_uri: getHostUrl('/auth/callback'),
-        grant_type: 'authorization_code'
-    });
-    request.post(tokenRequestOptions, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            res.redirect(getClientUrl(config.client.successPath 
-                + `/${body.access_token}/${body.refresh_token}`));
-        } else {
-            res.redirect(getClientUrl(config.client.errorPath)
-                + querystring.stringify({ error: 'invalid_token' }));
-        }
-    });
-}
 
 export default router;
--- server/spotify/auth/util.js
+++ server/spotify/auth/util.js
@@ -2,33 +2,35 @@ import config from '../../config';
 
 export const generateRandomState = (length) => {
     let state = '';
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < length; i++) {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        + 'abcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i += 1) {
         state += letters.charAt(Math.floor(Math.random() * letters.length));
     }
     return state;
 };
 
-export const getSpotifyAuthUrl = (urlPath) => `${config.spotify.authBaseUri}${urlPath}`;
+export const getSpotifyAuthUrl =
+    urlPath => `${config.spotify.authBaseUri}${urlPath}`;
 
 export const getHostUrl = (urlPath) => {
-    if (config.host.production)
-        return `${config.host.baseUri}${urlPath}`;
+    if (config.host.production) return `${config.host.baseUri}${urlPath}`;
     return `${config.host.baseUri}:${config.host.port}${urlPath}`;
 };
 
-export const getClientUrl = (urlPath) => `${config.client.baseUri}${urlPath}`;
+export const getClientUrl = urlPath => `${config.client.baseUri}${urlPath}`;
 
 const getApplicationToken = () => {
-    const tokenToEncrypt = `${config.spotify.clientId}:${config.spotify.clientSecret}`
-    return `Basic ${new Buffer(tokenToEncrypt).toString('base64')}`;
-}
+    const tokenToEncrypt =
+        `${config.spotify.clientId}:${config.spotify.clientSecret}`;
+    return `Basic ${Buffer.from(tokenToEncrypt).toString('base64')}`;
+};
 
-export const getTokenReqOptions = (form) => ({
+export const getTokenReqOptions = form => ({
     url: getSpotifyAuthUrl('/api/token'),
     headers: {
-        'Authorization': getApplicationToken()
+        Authorization: getApplicationToken(),
     },
-    form: form,
-    json: true
+    form,
+    json: true,
 });
