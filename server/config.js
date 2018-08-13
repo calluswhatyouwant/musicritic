d325cde535168289d07845b6ba5a9a3653314aed
José Renan <joserenansl99@gmail.com>
2018-08-13 16:54:07 -0300

    Fix problem with firebase and heroku

--- server/config.js
+++ server/config.js
@@ -32,9 +32,9 @@ config.client = {
 };
 
 const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
 };
 
 config.firebase = {
