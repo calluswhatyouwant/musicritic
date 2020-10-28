282aa4dd96974627f65bca4a300acd3661263e77
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-10-28 12:51:31 -0300

    Add route for users to get their own track review

--- server/src/track-reviews/trackReviewController.js
+++ server/src/track-reviews/trackReviewController.js
@@ -3,9 +3,24 @@
 import express from 'express';
 import { uniqueId } from 'lodash';
 import checkAuth from '../firebase/firebaseAuthHandler';
-import { createTrackReview } from './trackReviewCollections';
+import {
+    createTrackReview,
+    getUserTrackReview,
+} from './trackReviewCollections';
 
 const router = express.Router();
+
+router.get('/track/:id/reviews/me', checkAuth, (req, res) => {
+    const trackId = req.params.id;
+    const authorUid = req.user.uid;
+    getUserTrackReview(trackId, authorUid)
+        .then(reviews => {
+            reviews.size === 0
+                ? res.status(204).send()
+                : res.status(200).send(reviews.docs[0]);
+        })
+        .catch(error => res.status(error.status).send(error));
+});
 
 router.post('/track/:id/reviews', checkAuth, (req, res) => {
     const review = req.body;
