346130ed3769103dbc1995a1a33498f0dd54fad1
José Renan <jose.renan@splab.ufcg.edu.br>
2020-10-30 20:26:06 -0300

    Add route to update track reviews

--- server/src/track-reviews/trackReviewController.js
+++ server/src/track-reviews/trackReviewController.js
@@ -6,12 +6,13 @@ import checkAuth from '../firebase/firebaseAuthHandler';
 import {
     createTrackReview,
     getUserTrackReview,
+    updateUserTrackReview,
 } from './trackReviewCollections';
 
 const router = express.Router();
 
-router.get('/track/:id/reviews/me', checkAuth, (req, res) => {
-    const trackId = req.params.id;
+router.get('/track/:trackId/reviews/me', checkAuth, (req, res) => {
+    const trackId = req.params.trackId;
     const authorUid = req.user.uid;
     getUserTrackReview(trackId, authorUid)
         .then(reviews => {
@@ -22,12 +23,22 @@ router.get('/track/:id/reviews/me', checkAuth, (req, res) => {
         .catch(error => res.status(error.status).send(error));
 });
 
-router.post('/track/:id/reviews', checkAuth, (req, res) => {
+router.post('/track/:trackId/reviews', checkAuth, (req, res) => {
     const review = req.body;
-    review.trackId = req.params.id;
+    review.trackId = req.params.trackId;
     review.authorUid = req.user.uid;
     createTrackReview(review)
         .then(review => res.status(201).send(review))
+        .catch(error => res.status(error.status).send(error));
+});
+
+router.put('/track/:trackId/reviews/:reviewId', checkAuth, (req, res) => {
+    const review = req.body;
+    review.trackId = req.params.trackId;
+    review.id = req.params.reviewId;
+    review.authorUid = req.user.uid;
+    updateUserTrackReview(review.id, review)
+        .then(review => res.status(200).send(review))
         .catch(error => res.status(error.status).send(error));
 });
 
