cf43bef5bee2396784b94c38197dadbef4473d13
José Renan <jose.renan@splab.ufcg.edu.br>
2020-10-30 20:26:06 -0300

    Add route to update track reviews

--- server/src/track-reviews/trackReviewCollections.js
+++ server/src/track-reviews/trackReviewCollections.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+import { reduce } from 'lodash';
 import { db } from '../firebase/firebaseAdmin';
 
 type TrackReviewModel = {
@@ -33,4 +34,32 @@ export const getUserTrackReview = (trackId: string, authorUid: string) => {
     return TrackReviews.where('authorUid', '==', authorUid)
         .where('trackId', '==', trackId)
         .get();
+};
+
+export const updateUserTrackReview = async (
+    reviewId: string,
+    updatedTrackReview: TrackReviewModel
+) => {
+    const ref = TrackReviews.doc(reviewId);
+    const review = (await ref.get()).data();
+    if (!review)
+        throw {
+            status: 500,
+            message: 'Review not found',
+        };
+
+    if (updatedTrackReview.authorUid !== review.authorUid)
+        throw {
+            status: 500,
+            message: 'Not authorized to edit review from others',
+        };
+
+    if (updatedTrackReview.trackId !== review.trackId)
+        throw {
+            status: 500,
+            message: 'Cannot change the track which is being reviewed',
+        };
+
+    ref.set(updatedTrackReview);
+    return updatedTrackReview;
 };
