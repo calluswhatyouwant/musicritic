30f86c7b57e1100bf4d238317afb973bded5a17c
marianabianca <maridbianca@gmail.com>
2020-11-01 11:31:31 -0300

    Update code to fix lint problem

--- musicritic/src/api/UserAPI.js
+++ musicritic/src/api/UserAPI.js
@@ -6,26 +6,8 @@ export const userApi = axios.create({
     baseURL: process.env.SERVER_BASE_URL,
 });
 
-export const userApiauthenticated = axios.create({
-    baseURL: process.env.SERVER_BASE_URL,
-    headers: {
-        Authorization: localStorage.getItem('authToken')
-    }
-});
-
 export const createUser = (user: any) => userApi.post('/users', user)
     .then(result => result.data)
     .catch((error) => {
         throw error.response.data;
     });
-
-export const postTrackRating = (
-    trackID: string,
-    rating: number
-    ) => {
-    userApiauthenticated.post(`track/${trackID}/reviews`, { rating })
-    .then(result => result.data)
-    .catch((error) => {
-        throw error.response.data
-    })
-    }
