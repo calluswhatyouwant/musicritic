240a6e80c42a8498ad15bc70c962454667989e25
Robson Junior <jose.robson@splab.ufcg.edu.br>
2020-11-30 17:07:05 -0300

    Fix lint error

--- musicritic/src/components/album/AlbumPage.js
+++ musicritic/src/components/album/AlbumPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import _ from 'lodash';
 
 import AlbumSummary from './summary/AlbumSummary';
 import ReviewSection from '../review/ReviewSection';
