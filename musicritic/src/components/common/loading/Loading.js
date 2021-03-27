b06f6d96c70c3adc9ff36fbf6a440ccf0fdd72b1
Robson Junior <jose.robson@splab.ufcg.edu.br>
2021-03-26 21:43:39 -0300

    Update Loading component

--- musicritic/src/components/common/loading/Loading.js
+++ musicritic/src/components/common/loading/Loading.js
@@ -7,7 +7,8 @@ import './Loading.css';
 const Loading = () => (
     <div className="p-5">
         <div className="d-flex justify-content-center">
-            <div className="spinner spinner-border" role="status">
+            <div className="rotate" role="status">
+                <i className="fas fa-compact-disc" />
                 <span className="sr-only">Loading...</span>
             </div>
         </div>
