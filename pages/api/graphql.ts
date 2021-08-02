60f688a7ca4e2784a3211bf810ff8f33a82793db
José Renan <joserenansl99@gmail.com>
2021-08-01 22:08:31 -0300

    update apollo server version

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server-micro'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 import typeDefs from '@/graphql/schema.graphql'
 import resolvers from '@/graphql/resolvers'
@@ -16,4 +17,29 @@ export const config = {
   },
 }
 
-export default apolloServer.createHandler({ path: '/api/graphql' })
+const startServer = apolloServer.start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  if (req.method === 'OPTIONS') {
+    res.end()
+
+    return false
+  }
+
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
